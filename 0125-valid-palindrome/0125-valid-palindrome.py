ecff50ab32f6d7376f701a5082375300b2e5b295
Minho Lee <65007049+minho-lee0716@users.noreply.github.com>
2025-01-24 20:04:11 +0900

    Time: 3 ms (98.79%), Space: 18.5 MB (34.11%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,13 @@
+# class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         s = ''.join([char for char in s if char.isalnum()]).lower()
+#         for i in range(len(s)//2):
+#             if s[i] != s[~i]:
+#                 return False
+#         return True
+
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = ''.join([char for char in s if char.isalnum()]).lower()
-        for i in range(len(s)//2):
-            if s[i] != s[~i]:
-                return False
-        return True
+        size = len(s)
+        return s[:size//2] == s[size//2:][::-1] if size % 2 == 0 else s[:size//2] == s[size//2 + 1:][::-1]
