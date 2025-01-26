285bc175ebbe68a4253fc0cbbc091ddb13a8197b
Minho Lee <65007049+minho-lee0716@users.noreply.github.com>
2025-01-26 14:51:38 +0900

    Time: 3 ms (98.54%), Space: 17.9 MB (40.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -8,18 +8,23 @@
 #                 return False
 #         return True
 
+# class Solution:
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         temp = []
+#         for char in s:
+#             if char in temp:
+#                 continue
+#             if s.count(char) != t.count(char):
+#                 return False
+#             temp.append(char)
+#         return True
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-
-        temp = []
-        for char in s:
-            if char in temp:
-                continue
-
-            if s.count(char) != t.count(char):
+        while (len(s) != 0):
+            if s.count(s[0]) != t.count(s[0]):
                 return False
-
-            temp.append(char)
+            s, t = s.replace(s[0], ''), t.replace(s[0], '')
+            if len(s) != len(t):
+                return False
         return True
