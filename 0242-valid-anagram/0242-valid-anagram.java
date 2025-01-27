e5c84a8dfeaeab6dde9227408b98b33afe63cfc2
Minho Lee <65007049+minho-lee0716@users.noreply.github.com>
2025-01-28 00:01:10 +0900

    Time: 19 ms (15.02%), Space: 44.6 MB (45.29%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,24 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        // 문자열 길이가 다르면 애너그램이 될 수 없음
         if (s.length() != t.length()) {
             return false;
         }
 
         while (s.length() > 0) {
-            char curr = s.charAt(0); // 첫 번째 문자 가져오기
-            long countS = s.chars().filter(c -> c == curr).count(); // s에서 문자 개수 세기
-            long countT = t.chars().filter(c -> c == curr).count(); // t에서 문자 개수 세기
+            char curr = s.charAt(0);
+            long countS = s.chars().filter(c -> c == curr).count();
+            long countT = t.chars().filter(c -> c == curr).count();
 
             if (countS != countT) {
-                return false; // 개수가 다르면 애너그램 아님
+                return false;
             }
 
-            // 해당 문자 제거
             s = s.replace(Character.toString(curr), "");
             t = t.replace(Character.toString(curr), "");
 
-            // 문자열 길이 다시 확인
             if (s.length() != t.length()) {
                 return false;
             }
