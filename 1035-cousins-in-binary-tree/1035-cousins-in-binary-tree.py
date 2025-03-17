897194367f2347a9882f6786a8a414224c622984
Minho Lee <65007049+minho-lee0716@users.noreply.github.com>
2025-03-17 19:46:34 +0900

    Time: 0 ms (100%), Space: 17.7 MB (80.91%) - LeetHub

---
+++ 1035-cousins-in-binary-tree/1035-cousins-in-binary-tree.py
@@ -0,0 +1,29 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
+        node_x, node_y = [None, 0, False], [None, 0, False]
+        def wrap(node: Optional[TreeNode], parent: int, depth: int):
+            if not node:
+                return
+
+            if node.val == x:
+                node_x[0], node_x[1], node_x[2] = parent, depth, True
+
+            if node.val == y:
+                node_y[0], node_y[1], node_y[2] = parent, depth, True
+
+            if node_x[2] and node_y[2]:
+                return
+
+            wrap(node.left, node.val, depth + 1)
+            wrap(node.right, node.val, depth + 1)
+
+
+        wrap(root, None, 0)
+        return (node_x[0] != node_y[0]) and (node_x[1] == node_y[1])
+
